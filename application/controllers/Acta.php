b7af5f1f526b3a785e0d3e59074792d774341376
Alberto S. Castaneda <alb2594@gmail.com>
2018-09-09 13:51:09 -0600

    genera acta denegado, observacion y prevenido

--- application/controllers/Acta.php
+++ application/controllers/Acta.php
@@ -15,9 +15,6 @@ class Acta extends CI_Controller {
         switch ($data['id_estadort']) {
             case 3:
                 switch ($data['tiposolicitud_expedientert']) {
-                    case 'Registro':
-                        $this->generar_acta_aprobar($id);
-                        break;
                     case 'Reforma Parcial':
                         $this->generar_acta_aprobar_parcial($id);
                         break;
@@ -25,12 +22,28 @@ class Acta extends CI_Controller {
                         $this->generar_acta_aprobar_total($id);
                         break;
                     default:
-                        # code...
+                        $this->generar_acta_aprobar($id);
                         break;
                 }
                 break;
             case 6:
-                $this->generar_acta_denegada($id);
+                switch ($data['tiposolicitud_expedientert']) {
+                    case 'Reforma Parcial':
+                        $this->generar_acta_denegada_parcial($id);
+                        break;
+                    case 'Reforma Total':
+                        $this->generar_acta_denegada_total($id);
+                        break;                    
+                    default:
+                        $this->generar_acta_denegada($id);
+                        break;
+                }
+                break;
+            case 2:
+                $this->generar_acta_observado($id);
+                break;
+            case 4:
+                $this->generar_acta_prevenido($id);
                 break;
             default:
                 # code...
@@ -107,6 +120,9 @@ class Acta extends CI_Controller {
         $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
         $templateWord->setValue('horas_letras', hora(date('G')));
         $templateWord->setValue('hora_crea', hora(date('G', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('dia_crea', dia(date('d', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('mes_crea', strtoupper(mes(date('m', strtotime($expediente->fechacrea_expedientert)))));
+        $templateWord->setValue('anio_crea', anio(date('Y', strtotime($expediente->fechacrea_expedientert))));
         $templateWord->setValue('dias_letras', dia(date('d')));
         $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
         $templateWord->setValue('anio_letras', anio(date('Y')));
@@ -162,10 +178,10 @@ class Acta extends CI_Controller {
         $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sarit/files/templates/estadoAprobadoRefTotal/APROBACION_REFORMATOTAL-expediente.docx');
         $templateWord->setValue('numero', $expediente->numexpediente_expedientert);
         $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
-        $templateWord->setValue('horas_letras', date('G'));
-        $templateWord->setValue('dias_letras', date('d'));
+        $templateWord->setValue('horas_letras', hora(date('G')));
+        $templateWord->setValue('dias_letras', dia(date('d')));
         $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
-        $templateWord->setValue('anio_letras', date('Y'));
+        $templateWord->setValue('anio_letras', anio(date('Y')));
         $templateWord->setValue('hora_crea', hora(date('G', strtotime($expediente->fechacrea_expedientert))));
         $templateWord->setValue('dia_crea', dia(date('d', strtotime($expediente->fechacrea_expedientert))));
         $templateWord->setValue('mes_crea', strtoupper(mes(date('m', strtotime($expediente->fechacrea_expedientert)))));
@@ -223,10 +239,10 @@ class Acta extends CI_Controller {
         $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sarit/files/templates/estadoDenegado/ADEE_NUEVOREGLAMENTO-expediente.docx');
         $templateWord->setValue('numero', $expediente->numexpediente_expedientert);
         $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
-        $templateWord->setValue('horas_letras', date('G'));
-        $templateWord->setValue('dias_letras', date('d'));
+        $templateWord->setValue('horas_letras', hora(date('G')));
+        $templateWord->setValue('dias_letras', dia(date('d')));
         $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
-        $templateWord->setValue('anio_letras', date('Y'));
+        $templateWord->setValue('anio_letras', anio(date('Y')));
         $templateWord->setValue('hora_crea', hora(date('G', strtotime($expediente->fechacrea_expedientert))));
         $templateWord->setValue('dia_crea', dia(date('d', strtotime($expediente->fechacrea_expedientert))));
         $templateWord->setValue('mes_crea', strtoupper(mes(date('m', strtotime($expediente->fechacrea_expedientert)))));
@@ -271,6 +287,222 @@ class Acta extends CI_Controller {
 
     }
 
+    public function generar_acta_denegada_parcial($id) {
+
+        $expediente = $this->reglamento_model->obtener_reglamento_empresa( $id )->result()[0];
+
+        $jefe = $this->reglamento_model->jefe_direccion_trabajo()->result()[0];
+        
+        $this->load->library("phpword");
+
+        $PHPWord = new PHPWord();
+
+        $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sarit/files/templates/estadoDenegadoRefParcial/ADEE_REFORMAPARCIAL-expediente.docx');
+        $templateWord->setValue('numero', $expediente->numexpediente_expedientert);
+        $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
+        $templateWord->setValue('horas_letras', hora(date('G')));
+        $templateWord->setValue('dias_letras', dia(date('d')));
+        $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
+        $templateWord->setValue('anio_letras', anio(date('Y')));
+        $templateWord->setValue('hora_crea', hora(date('G', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('dia_crea', dia(date('d', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('mes_crea', strtoupper(mes(date('m', strtotime($expediente->fechacrea_expedientert)))));
+        $templateWord->setValue('anio_crea', anio(date('Y', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('adjetivo_representante', $this->adjetivo_genero($expediente->sexo_representantert));
+        $templateWord->setValue('nombre_representante', $expediente->nombres_representantert . ' ' . $expediente->apellidos_representantert);
+        $templateWord->setValue('cargo_representante', $expediente->cargo_representantert);
+        $templateWord->setValue('tipo_persona', $expediente->tipopersona_expedientert);
+        $templateWord->setValue('nombre_empresa', $expediente->nombre_empresa);
+        $templateWord->setValue('abr_empresa', $expediente->abreviatura_empresa);
+        $templateWord->setValue('dirección_empresa', $expediente->direccion_empresa);
+        $templateWord->setValue('muni_empresa', $expediente->municipio);
+        $templateWord->setValue('muni_empresa', $expediente->municipio);
+        $templateWord->setValue('dia_resolucion', dia(date('d', strtotime($expediente->fecharesolucion_expedientert))));
+        $templateWord->setValue('mes_resolucion', strtoupper(mes(date('m', strtotime($expediente->fecharesolucion_expedientert)))));
+        $templateWord->setValue('anio_resolucion', anio(date('Y', strtotime($expediente->fecharesolucion_expedientert))));
+        //$templateWord->setValue('contenido', $expediente->contenidoTitulos_expedientert);
+        //$templateWord->setValue('estudio_diretor', $jefe->abr_nivel_academico);
+        //$templateWord->setValue('nombre_director', $jefe->nombre_completo_jefa);
+        $templateWord->setValue('nombre_delegado', 
+                                $expediente->primer_nombre . ' '
+                                . $expediente->segundo_nombre . ' '
+                                . $expediente->tercer_nombre . ' '
+                                . $expediente->primer_apellido . ' '
+                                . $expediente->segundo_apellido . ' '
+                                . $expediente->apellido_casada);
+
+        $nombreWord = $this->random();
+
+        $templateWord->saveAs($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        $phpWord2 = \PhpOffice\PhpWord\IOFactory::load($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        header("Content-Disposition: attachment; filename='denegacion_reglamento_parcial_".date('dmy_His').".docx'");
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord2, 'Word2007');
+        $objWriter->save('php://output');
+
+        unlink($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+    }
+    
+    public function generar_acta_denegada_total($id) {
+
+        $expediente = $this->reglamento_model->obtener_reglamento_empresa( $id )->result()[0];
+
+        $jefe = $this->reglamento_model->jefe_direccion_trabajo()->result()[0];
+        
+        $this->load->library("phpword");
+
+        $PHPWord = new PHPWord();
+
+        $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sarit/files/templates/estadoDenegadoRefTotal/ADEE_REFORMATOTAL-expediente.docx');
+        $templateWord->setValue('numero', $expediente->numexpediente_expedientert);
+        $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
+        $templateWord->setValue('horas_letras', hora(date('G')));
+        $templateWord->setValue('dias_letras', dia(date('d')));
+        $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
+        $templateWord->setValue('anio_letras', anio(date('Y')));
+        $templateWord->setValue('hora_crea', hora(date('G', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('dia_crea', dia(date('d', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('mes_crea', strtoupper(mes(date('m', strtotime($expediente->fechacrea_expedientert)))));
+        $templateWord->setValue('anio_crea', anio(date('Y', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('adjetivo_representante', $this->adjetivo_genero($expediente->sexo_representantert));
+        $templateWord->setValue('nombre_representante', $expediente->nombres_representantert . ' ' . $expediente->apellidos_representantert);
+        $templateWord->setValue('cargo_representante', $expediente->cargo_representantert);
+        $templateWord->setValue('tipo_persona', $expediente->tipopersona_expedientert);
+        $templateWord->setValue('nombre_empresa', $expediente->nombre_empresa);
+        $templateWord->setValue('abr_empresa', $expediente->abreviatura_empresa);
+        $templateWord->setValue('dirección_empresa', $expediente->direccion_empresa);
+        $templateWord->setValue('muni_empresa', $expediente->municipio);
+        $templateWord->setValue('muni_empresa', $expediente->municipio);
+        $templateWord->setValue('dia_resolucion', dia(date('d', strtotime($expediente->fecharesolucion_expedientert))));
+        $templateWord->setValue('mes_resolucion', strtoupper(mes(date('m', strtotime($expediente->fecharesolucion_expedientert)))));
+        $templateWord->setValue('anio_resolucion', anio(date('Y', strtotime($expediente->fecharesolucion_expedientert))));
+        //$templateWord->setValue('contenido', $expediente->contenidoTitulos_expedientert);
+        //$templateWord->setValue('estudio_diretor', $jefe->abr_nivel_academico);
+        //$templateWord->setValue('nombre_director', $jefe->nombre_completo_jefa);
+        $templateWord->setValue('nombre_delegado', 
+                                $expediente->primer_nombre . ' '
+                                . $expediente->segundo_nombre . ' '
+                                . $expediente->tercer_nombre . ' '
+                                . $expediente->primer_apellido . ' '
+                                . $expediente->segundo_apellido . ' '
+                                . $expediente->apellido_casada);
+
+        $nombreWord = $this->random();
+
+        $templateWord->saveAs($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        $phpWord2 = \PhpOffice\PhpWord\IOFactory::load($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        header("Content-Disposition: attachment; filename='denegacion_reglamento_total_".date('dmy_His').".docx'");
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord2, 'Word2007');
+        $objWriter->save('php://output');
+
+        unlink($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+    }
+
+    public function generar_acta_observado($id) {
+
+        $expediente = $this->reglamento_model->obtener_reglamento_empresa( $id )->result()[0];
+
+        $jefe = $this->reglamento_model->jefe_direccion_trabajo()->result()[0];
+        
+        $this->load->library("phpword");
+
+        $PHPWord = new PHPWord();
+
+        $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sarit/files/templates/estadoObservado/OBSERVACION_NUEVOREGLAMENTO-expediente.docx');
+        $templateWord->setValue('numero', $expediente->numexpediente_expedientert);
+        $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
+        $templateWord->setValue('horas_letras', hora(date('G')));
+        $templateWord->setValue('dias_letras', dia(date('d')));
+        $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
+        $templateWord->setValue('anio_letras', anio(date('Y')));
+        $templateWord->setValue('tipo_persona', $expediente->tipopersona_expedientert);
+        $templateWord->setValue('nombre_empresa', $expediente->nombre_empresa);
+        $templateWord->setValue('abr_empresa', $expediente->abreviatura_empresa);
+        $templateWord->setValue('estudio_diretor', $jefe->abr_nivel_academico);
+        $templateWord->setValue('nombre_director', $jefe->nombre_completo_jefa);
+
+        $nombreWord = $this->random();
+
+        $templateWord->saveAs($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        $phpWord2 = \PhpOffice\PhpWord\IOFactory::load($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        header("Content-Disposition: attachment; filename='observado_reglamento_".date('dmy_His').".docx'");
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord2, 'Word2007');
+        $objWriter->save('php://output');
+
+        unlink($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+    }
+
+    public function generar_acta_prevenido($id) {
+
+        $expediente = $this->reglamento_model->obtener_reglamento_empresa( $id )->result()[0];
+
+        $jefe = $this->reglamento_model->jefe_direccion_trabajo()->result()[0];
+        
+        $this->load->library("phpword");
+
+        $PHPWord = new PHPWord();
+
+        $templateWord = $PHPWord->loadTemplate($_SERVER['DOCUMENT_ROOT'].'/sarit/files/templates/estadoPrevenido/PREVENCION_NUEVOREGLAMENTO-expediente.docx');
+        $templateWord->setValue('numero', $expediente->numexpediente_expedientert);
+        $templateWord->setValue('municipio', $this->municipio($expediente->numexpediente_expedientert));
+        $templateWord->setValue('horas_letras', hora(date('G')));
+        $templateWord->setValue('dias_letras', dia(date('d')));
+        $templateWord->setValue('mes_letras', strtoupper(mes(date('m'))));
+        $templateWord->setValue('anio_letras', anio(date('Y')));
+        $templateWord->setValue('hora_crea', hora(date('G', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('dia_crea', dia(date('d', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('mes_crea', strtoupper(mes(date('m', strtotime($expediente->fechacrea_expedientert)))));
+        $templateWord->setValue('anio_crea', anio(date('Y', strtotime($expediente->fechacrea_expedientert))));
+        $templateWord->setValue('adjetivo_representante', $this->adjetivo_genero($expediente->sexo_representantert));
+        $templateWord->setValue('nombre_representante', $expediente->nombres_representantert . ' ' . $expediente->apellidos_representantert);
+        $templateWord->setValue('cargo_representante', $expediente->cargo_representantert);
+        $templateWord->setValue('tipo_persona', $expediente->tipopersona_expedientert);
+        $templateWord->setValue('nombre_empresa', $expediente->nombre_empresa);
+        $templateWord->setValue('abr_empresa', $expediente->abreviatura_empresa);
+        //$templateWord->setValue('contenido', $expediente->contenidoTitulos_expedientert);
+        //$templateWord->setValue('estudio_diretor', $jefe->abr_nivel_academico);
+        //$templateWord->setValue('nombre_director', $jefe->nombre_completo_jefa);
+        $templateWord->setValue('nombre_delegado', 
+                                $expediente->primer_nombre . ' '
+                                . $expediente->segundo_nombre . ' '
+                                . $expediente->tercer_nombre . ' '
+                                . $expediente->primer_apellido . ' '
+                                . $expediente->segundo_apellido . ' '
+                                . $expediente->apellido_casada);
+
+        $nombreWord = $this->random();
+
+        $templateWord->saveAs($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        $phpWord2 = \PhpOffice\PhpWord\IOFactory::load($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+        header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        header("Content-Disposition: attachment; filename='prevencion_reglamento_".date('dmy_His').".docx'");
+        header('Cache-Control: max-age=0');
+
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord2, 'Word2007');
+        $objWriter->save('php://output');
+
+        unlink($_SERVER['DOCUMENT_ROOT'].'/sirct/files/generate/'.$nombreWord.'.docx');
+
+    }
 
     private function municipio($numero_expediente) {
         $abr = substr($numero_expediente, -2);
